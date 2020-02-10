973924c9f280692b4a4951cdf20b7e5d9297e03c
Владислав Лякишев <vlad169@yandex.ru>
2020-02-10 21:06:42 +0300

    Complite

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 Route::get('/', 'HomeController@index')->name('home');
 
 Route::get('/news', 'NewsController@index')->name('news');
-Route::get('news/{id}', 'NewsController@inCategory');
-Route::get('onenews/{id}', 'NewsController@oneNews');
+Route::get('/news/{id}', 'NewsController@inCategory');
+Route::get('/onenews/{id}', 'NewsController@oneNews');
