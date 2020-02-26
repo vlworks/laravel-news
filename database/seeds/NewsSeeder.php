0e3a71ed9a14b8e0a1b15be6ffdad8fa5858f443
Владислав Лякишев <vlad169@yandex.ru>
2020-02-26 10:58:05 +0300

    Add seeds

--- database/seeds/NewsSeeder.php
+++ database/seeds/NewsSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Faker\Factory;
 
 class NewsSeeder extends Seeder
 {
@@ -11,6 +12,21 @@ class NewsSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('news')->insert($this->getData());
+    }
 
+    private function getData()
+    {
+        $faker = Faker\Factory::create('ru_RU');
+        $data = [];
+        for ($i = 0; $i < 10; $i++) {
+            $data[] = [
+                'title' => $faker->realText(rand(20, 50)),
+                'text' => $faker->realText(rand(1000, 2000)),
+                'isPrivate' => false,
+                'image' => 'default'
+            ];
+        }
+        return $data;
     }
 }
