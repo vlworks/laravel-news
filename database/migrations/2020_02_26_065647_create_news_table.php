efa22ce305bfed90f3c32e69b69b07e1712ddfdb
Владислав Лякишев <vlad169@yandex.ru>
2020-02-26 10:00:58 +0300

    Start les5

---
+++ database/migrations/2020_02_26_065647_create_news_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned();
+            $table->string('title')->comment('Заголовок новости');
+            $table->text('text')->comment('Содержание новости');
+            $table->boolean('isPrivate')
+                ->default(false);
+            //$table->string('image'); доделать
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+}
