14d3aa4fc04234be097019d80356d53ec3c8481a
Владислав Лякишев <vlad169@yandex.ru>
2020-02-29 14:18:37 +0300

    Готова к сдаче ДЗ

--- app/News.php
+++ app/News.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     protected $fillable = ['title', 'text', 'category_id', 'isPrivate', 'image'];
+
+    public  function category() {
+        return $this->belongsTo(Category::class, 'category_id')->first();
+    }
 }
