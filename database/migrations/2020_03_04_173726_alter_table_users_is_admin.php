752d7d20b0c4fae496cf46ed388f142a0a1ba656
unknown <vlad169@yandex.ru>
2020-03-10 15:16:02 +0300

    Start oauth facebook

--- database/migrations/2020_03_04_173726_alter_table_users_is_admin.php
+++ database/migrations/2020_03_04_173726_alter_table_users_is_admin.php
@@ -18,7 +18,7 @@ class AlterTableUsersIsAdmin extends Migration
                 ->default(false)
                 ->comment('Админ ли это');
         });
-        (new UserSeeder())->run();
+        //(new UserSeeder())->run();
     }
 
     /**
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(NewsSeeder::class);
-        //$this->call(UserSeeder::class);
+        $this->call(UserSeeder::class);
         //$this->call(CategorySeeder::class);
     }
 }
